5b064378b738a46cdc46b2c923d30e459000faa5
Felipe <12felipexd@gmail.com>
2025-10-08 20:10:59 -0300

    '08/10/2025'

---
+++ Brasileirao/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	char *time[5] = {"Time1", "Time2", "Time3", "Time4", "Time5"};
+	int i;
+	for (i = 0; i < 3; i++){
+		printf("O %d° eh: %s\n", i+1, time[i] );
+	}
+	
+	return 0;
+}
