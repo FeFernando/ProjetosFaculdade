8a820e1175314f3141ea3784f3cd0da29648352f
Felipe <12felipexd@gmail.com>
2025-10-23 20:01:57 -0300

    '23/10/2025'

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	float num1, num2;
+	char operador;
+	printf("Digite a operação (ex: 5 * 2 ): ");
+	scanf("%f %s %f", &num1, &operador, &num2);
+	
+	
+	
+	
+	
+	
+	return 0;
+}
